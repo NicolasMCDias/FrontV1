cc44a18a61c84791c1132c7a44cd8d8d76ffa886
unknown <nicolasmatheuscd@hotmail.com>
2023-10-16 22:04:11 -0300

    Primeiro

---
+++ FrontTest/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {ref} from 'vue'
+
+let num = ref(0)
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark boder-bottom boder-body">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">("NOME de algo")</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data_bs_target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/Login" class="nav-link" aria-current="page">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/Cadastre-se" class="nav-link">Cadastre-se</router-link>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+  <div class="main">
+    <router-view></router-view>
+  </div>
+
+  <footer class="bg-dark text-light">
+    <div> Unimar | ADS | {{ new Date().getFullYear() }}
+    </div>
+  </footer>
+</template>
+
+<style>
+
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: 100;
+}
+</style>
---
+++ FrontTest/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import {ref} from 'vue'
+
+let num = ref(0)
+
+</script>
+
+<template>
+ <div class="card" style="width: 25rem;display: grid;place-items: center;">
+  <div class="card-header" style="border: 100%;">
+    <h4>Login:</h4>
+  </div>
+  <div class="card-body">
+    <h6 class="card-title">CPF</h6>
+    <input class="form-control" type="text" placeholder="Inserir CPF" aria-label="default input example">
+    <!--Area da senha-->
+    <br>
+    <label for="inputPassword5" class="form-label">Senha:</label>
+    <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" placeholder="Inserir Senha">
+  </div>
+  <div>
+      <router-link href="#" class="btn btn-primary"  type="password" style="position:static;left:50%;transform: translate(-50%,-50%);">Entrar</router-link >
+  </div> 
+ </div>
+</template>
